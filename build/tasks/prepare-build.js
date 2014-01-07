12b353b103be2fe837e8c5acf6821d1888df5082
xzyfer <xzyfer@gmail.com>
2014-01-07 14:15:13 +1100

    feat(build): themes need to run each components prepare

--- build/tasks/prepare-build.js
+++ build/tasks/prepare-build.js
@@ -2,7 +2,40 @@
 module.exports = function (grunt) {
     'use strict';
 
-    var shell = require('shelljs');
+    var shell = require('shelljs'),
+        path = require('path'),
+        meta = grunt.file.readJSON('./bower.json'),
+        isTheme = meta.name.indexOf('-theme-') !== -1;
+
+    function prepareTheme() {
+        var dep,
+            taskDir = function(dep) {
+                return path.join('bower_components', dep, 'build/tasks');
+            };
+
+        for (dep in meta.dependencies) {
+            if (!grunt.file.exists(path.join(taskDir(dep), 'prepare.js'))) {
+                continue;
+            }
+
+            grunt.task.loadTasks(taskDir(dep));
+            grunt.task.renameTask('prepare', 'prepare-' + dep);
+            grunt.task.run('prepare-' + dep);
+        }
+    }
+
+    function prepareComponent(done) {
+        shell.exec('npm install', function(code, output) {
+            if (code !== 0) {
+                grunt.log.error(output);
+                done();
+            }
+
+            grunt.loadTasks('build/tasks');
+            grunt.task.run('prepare');
+            done();
+        });
+    }
 
     grunt.registerTask(
         'prepare-build',
@@ -12,18 +45,11 @@ module.exports = function (grunt) {
                 return;
             }
 
-            var done = this.async();
-
-            shell.exec('npm install', function(code, output) {
-                if (code !== 0) {
-                    grunt.log.error(output);
-                    done();
-                }
-
-                grunt.loadTasks('build/tasks');
-                grunt.task.run('prepare');
-                done();
-            });
+            if (isTheme) {
+                prepareTheme();
+            } else {
+                prepareComponent(this.async());
+            }
         }
     );
 };
