3fd2d011fa9bf870aa5254d1d2f9dd6037b60ea3
xzyfer <xzyfer@gmail.com>
2014-01-28 17:48:51 +1100

    chore(build): clean up grunt build

--- Gruntfile.js
+++ Gruntfile.js
@@ -199,16 +199,16 @@ module.exports = function(grunt) {
 
         autoprefixer: {
             options: {
-                browsers: ['last 2 version', 'ie 8', 'ie 9']
-                // browsers: [
-                //     'last 2 version',
-                //     'Firefox ESR',
-                //     'BlackBerry 10',
-                //     'Android 4',
-                //     'Explorer 8',
-                //     'Explorer 9',
-                //     'Opera 12.1'
-                // ]
+                // browsers: ['last 2 version', 'ie 8', 'ie 9']
+                browsers: [
+                    'last 2 version',
+                    'Firefox ESR',
+                    'BlackBerry 10',
+                    'Android 4',
+                    'Explorer 8',
+                    'Explorer 9',
+                    'Opera 12.1'
+                ]
             },
             dist: {
                 src: 'dist/css/*!(.min).css'
